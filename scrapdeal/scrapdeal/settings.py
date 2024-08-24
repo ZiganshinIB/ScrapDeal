8850049152e78b70db248f9b9158d5e21ed3e448
ZiganshinIB <elzig3012@gmail.com>
2024-08-24 08:59:14 +0300

    Refract settings

--- scrapdeal/scrapdeal/settings.py
+++ scrapdeal/scrapdeal/settings.py
@@ -1,4 +1,8 @@
+import os
 from pathlib import Path
+
+import dj_database_url
+from django.conf.global_settings import AUTHENTICATION_BACKENDS
 from environs import Env
 
 env = Env()
@@ -7,7 +11,7 @@ env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = env.str('DJANGO_SECRET_KEY', default='django-secret-key_*HbY^FViuygytGVRD$%^&&FYV')
+SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
@@ -26,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scrap.apps.ScrapConfig',
+    'rest_framework',
+    'phonenumber_field',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -43,7 +50,7 @@ ROOT_URLCONF = 'scrapdeal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -58,21 +65,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scrapdeal.wsgi.application'
 
+# If web server NGINX is used
+if env.list("NGINX_DOMAINS", []):
+    CSRF_TRUSTED_ORIGINS = env.list("NGINX_DOMAINS")
+    USE_X_FORWARDED_HOST = True
+    USE_X_FORWARDED_PORT = True
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database Standard or your DB_URL
+if env.str('DB_URL', ''):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=env.str('DB_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -88,30 +105,72 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# AUH_USER_MODEL = 'account.User'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+LOGIN_REDIRECT_URL = 'scrap:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_RENDER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+
+}
+
+# DJOSER = {
+#     'SERIALIZERS': {
+#         'user_create': 'cake.serializers.ClientRegistrationSerializer',
+#     }
+# }
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
+
+PHONENUMBER_DEFAULT_REGION = 'RU'
 
 USE_I18N = True
 
 USE_TZ = True
 
 # Login
-LOGIN_REDIRECT_URL = 'scrap:dashboard'
-LOGIN_URL = 'account:login'
-LOGOUT_URL = 'account:logout'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if env.str('EMAIL_HOST_USER', '') and env.str('EMAIL_HOST_PASSWORD', ''):
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
