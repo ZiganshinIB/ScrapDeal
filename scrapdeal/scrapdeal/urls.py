f53a344cea2655f687973803ab4cc739d941f47b
ZiganshinIB <elzig3012@gmail.com>
2024-08-26 15:12:16 +0300

    Add urls scrapdeal

--- scrapdeal/scrapdeal/urls.py
+++ scrapdeal/scrapdeal/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('scrap.urls', namespace='scrap'),),
     path('api/', include('account.api_urls', namespace='api_account'),),
+    path('api/', include('scrap.api_urls', namespace='api_scrap'),),
     path('account/', include('account.urls', namespace='account'),),
 ]
 
